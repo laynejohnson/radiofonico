607a8b3efc4b966c924c1b6ccdcc48d05fa6d80c
Layne Johnson <layne@findum.com>
2021-05-14 14:28:27 -0400

    Refactor setup

---
+++ radiofonico/Models/AudioPlayer.swift
@@ -0,0 +1,28 @@
+//
+//  AudioPlayer.swift
+//  radiofonico
+//
+//  Created by Layne Johnson on 5/14/21.
+//
+
+import Foundation
+
+// MARK: - Audio Player
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(_ soundName: String) {
+
+   let path = Bundle.main.path(forResource: soundName, ofType:nil)!
+
+   let url = URL(fileURLWithPath: path)
+
+   do {
+       // Create the audio player
+       audioPlayer = try AVAudioPlayer(contentsOf: url)
+       // Play the sound effect
+       audioPlayer?.play()
+   } catch {
+       print("Could not summon audio player")
+   }
+}
