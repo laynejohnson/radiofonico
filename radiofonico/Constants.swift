a8cfdd2684a1f759200ecb8c36498d7b7dd3a64b
Layne Johnson <layne@findum.com>
2021-08-07 09:13:42 -0400

    Add Benvenuti view controller, add constants file and struct

---
+++ radiofonico/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  radiofonico
+//
+//  Created by Layne Johnson on 8/7/21.
+//
+
+import Foundation
+
+struct Constants {
+    let musicPLayerSegue = "ToMusicPlayer"
+}
---
+++ radiofonico/Controllers/BenvenutiViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BenvenutiViewController.swift
+//  radiofonico
+//
+//  Created by Layne Johnson on 8/7/21.
+//
+
+import UIKit
+
+class BenvenutiViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        // Animation code here + timer
+        
+        performSegue(withIdentifier: , sender: <#T##Any?#>)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
