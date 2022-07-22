7dd4e37d1fc0e918085c5f7c831bc2ecb0f32baa
laynejohnson <layne@findum.com>
2022-07-22 12:20:13 -0400

    Rm comments, play/pause does not reset to first song

--- radiofonico/Controllers/RadioViewController.swift
+++ radiofonico/Controllers/RadioViewController.swift
@@ -5,36 +5,10 @@
 //  Created by Layne Johnson on 5/14/21.
 //  Copyright © 2021. All rights reserved.
 
-/*
- 
- Music compilation by Beats in Space Radio:
- 
- https://www.mixcloud.com/bisradio/bis-radio-show-1072-with-il-quadro-di-troisi-donato-dozzy-eva-geist/
- 
- TRACKLIST: 1. Mango-Bella d’estate 2. Lucio Dalla-Washington 3. Franco Battiato-Summer on a Solitary Beach 4. Lucio Battisti-Rilassati ed Ascolta 5. Matia Bazar-Palestina-1983 Ariston6. Krisma-Samora Club7. Paolo Tofani-Un Altro Universo 8. Alice-Chan-Son Egocentrique 9. Giuni Russo-Buenos Aires10. Enrico Ruggeri-Polvere CGD11. Anna Oxa-Uragano e Nuvole 12. Garbo-A Berlino Va Bene 13. Righeira-Disco Volante 14. Gaznevada-Agente Speciale 15. Mike Francis-Survivor16. Marcella Bella-Nell’Aria 17. Teresa De Sio-Voglia E Turna
- 
- */
-
 import UIKit
 import AVFoundation
 
-// MARK: - Dev TODOs:
-
-/*
- Features:
- // TODO: Add "Radio Fonico" to launch screen
- // TODO: Implement repeat function
- // TODO: Implement autoplay
- // TODO: Add background playback
- 
- Refactor:
- // TODO: Refactor play/pause to pause/play current song. Do not reset to [0].
- // TODO: Refactor to remove song return
- // TODO: Refactor with Song class
- 
- */
-
-class ViewController: UIViewController {
+class RadioViewController: UIViewController {
     
     // MARK: - IBOutlets
     
@@ -57,7 +31,7 @@ class ViewController: UIViewController {
     
     // MARK: - Variables
     
-    let italianRadio = ItalianRadioModel()
+    let italianRadio = RadioModel()
     var timer: Timer?
     
     let song = " "
@@ -76,21 +50,17 @@ class ViewController: UIViewController {
         
         elapsedTimeLabel.text = "00:00"
         songTimeLabel.text = "00:00"
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         
-        // Hide navigation bar.
         navigationController?.isNavigationBarHidden = true
     }
     
     // MARK: - Animations
     
     func animateButton(button: UIButton) {
-        
-        // Scale animation for buttons
         UIView.animate(withDuration: 0.4, animations: { () -> Void in
             button.transform = .init(scaleX: 1.25, y: 1.25)
         }) { (finished: Bool) -> Void in
@@ -102,19 +72,16 @@ class ViewController: UIViewController {
     
     // MARK: - UI Functions
     
-    // Set favorite button.
-    func setMano(status: Bool) {
-        
+    func setFavorite(status: Bool) {
         if status == true {
             manoButton.isSelected = true
-            
+        
         } else {
             manoButton.isSelected = false
         }
     }
     
     func getFormattedTime(timeInterval: TimeInterval) -> String {
-        
         let mins = timeInterval / 60
         let secs = timeInterval.truncatingRemainder(dividingBy: 60)
         let timeformatter = NumberFormatter()
@@ -126,19 +93,16 @@ class ViewController: UIViewController {
         guard let minsStr = timeformatter.string(from: NSNumber(value: mins)), let secsStr = timeformatter.string(from: NSNumber(value: secs)) else {
             return ""
         }
+        
         return "\(minsStr):\(secsStr)"
     }
     
     @objc func updateProgressBar() {
-        
         guard let audioPlayer = audioPlayer else { return }
         
         let duration = audioPlayer.duration
         
-        // Set progress bar.
         progressBar.value = Float(audioPlayer.currentTime)
-        
-        // Set label text.
         songTimeLabel.text = getFormattedTime(timeInterval: duration)
         elapsedTimeLabel.text = getFormattedTime(timeInterval: audioPlayer.currentTime)
     }
@@ -146,153 +110,95 @@ class ViewController: UIViewController {
     // MARK: - IBActions
     
     @IBAction func progressValueChanged(_ sender: UISlider) {
-        
         if italianRadio.isPlaying == true {
-            
             audioPlayer?.currentTime = Float64(progressBar.value)
         }
     }
     
-    @IBAction func playPreviousSong(_ sender: UIButton) {
-        
-        // Animate play/pause button if no song is playing.
-        if italianRadio.isPlaying == false && songLabel.text == "" {
-            
-            animateButton(button: playPauseButton)
-            
-        } else if italianRadio.isPlaying == true {
-            
-            // Choose song.
-            let song = italianRadio.playPreviousSong()
-            italianRadio.setSongLabel(song: song, songLabel: songLabel, artistLabel: artistLabel)
-            
-            // Check if song is a favorite.
-            let isFavorite = italianRadio.checkFavorite(song: song)
-            
-            // Set favorite button.
-            setMano(status: isFavorite)
-            
-        } else {
-            
-            // Animate play/pause button if no song is playing.
-            animateButton(button: playPauseButton)
-            
-        }
-    }
-    
-    @IBAction func playPauseRadio(_ sender: UIButton) {
-        
-        // Choose song.
-        let song = italianRadio.radioOnOff(sender: sender)
+    @IBAction func playPauseSong(_ sender: UIButton) {
+        let song = italianRadio.playPauseRadio(sender: sender)
         
         if song == defaultSongLabel {
-            
-            // Flash play button.
             animateButton(button: playPauseButton)
-            
+        
         } else {
-            
-            // Set song label.
             italianRadio.setSongLabel(song: song, songLabel: songLabel, artistLabel: artistLabel)
             
-            // Check song favorite status.
             let isFavorite = italianRadio.checkFavorite(song: song)
-            
-            // Set favorite button.
-            setMano(status: isFavorite)
+            setFavorite(status: isFavorite)
         }
         
         if italianRadio.isPlaying == true {
-            
-            // Set album art image.
             albumArt.image = #imageLiteral(resourceName: "album_art")
             
-            // Set progress bar values.
             progressBar.value = 0.0
             progressBar.maximumValue = Float(audioPlayer!.duration)
             
-            // Initiate timer.
             timer = Timer.scheduledTimer(timeInterval: 0.0001, target: self, selector: #selector(self.updateProgressBar), userInfo: nil, repeats: true)
         }
     }
     
-    @IBAction func playNextSong(_ sender: UIButton) {
-        
+    @IBAction func playPreviousSong(_ sender: UIButton) {
         if italianRadio.isPlaying == false && songLabel.text == "" {
-            
-            // Animate play/pause button if no song is playing.
             animateButton(button: playPauseButton)
             
         } else if italianRadio.isPlaying == true {
-            
-            // Get next song.
-            let song = italianRadio.playNextSong()
+            let song = italianRadio.playPreviousSong()
             italianRadio.setSongLabel(song: song, songLabel: songLabel, artistLabel: artistLabel)
-            
-            // Check favorite status.
             let isFavorite = italianRadio.checkFavorite(song: song)
-            
-            // Set favorite button.
-            setMano(status: isFavorite)
+            setFavorite(status: isFavorite)
             
         } else {
-            
-            // Animate play/pause button.
             animateButton(button: playPauseButton)
         }
     }
     
-    @IBAction func replaySong(_ sender: UIButton) {
-        
-        if songLabel.text == "" {
-            
-            // Animate play/pause button if no song is playing.
+    @IBAction func playNextSong(_ sender: UIButton) {
+        if italianRadio.isPlaying == false && songLabel.text == "" {
             animateButton(button: playPauseButton)
-        } else {
             
-            // Toggle replay button state.
+        } else if italianRadio.isPlaying == true {
+            let song = italianRadio.playNextSong()
+            italianRadio.setSongLabel(song: song, songLabel: songLabel, artistLabel: artistLabel)
+            let isFavorite = italianRadio.checkFavorite(song: song)
+
+            setFavorite(status: isFavorite)
+            
+        } else {
+            animateButton(button: playPauseButton)
+        }
+    }
+    
+    @IBAction func replayAllSongs(_ sender: UIButton) {
+        if songLabel.text == "" {
+            animateButton(button: playPauseButton)
+       
+        } else {
             sender.isSelected.toggle()
         }
+        
     }
     
     @IBAction func addFavorite(_ sender: UIButton) {
-        
         if songLabel.text == "" {
-            
-            // Animate play button.
             animateButton(button: playPauseButton)
-        }
-        
-        else if songLabel.text != nil && songLabel.text != defaultSongLabel && sender.isSelected == false {
             
-            // Set current song as favorite.
+        } else if songLabel.text != nil && songLabel.text != defaultSongLabel && sender.isSelected == false {
             sender.isSelected = true
-            
-            // Set song label.
+ 
             let song = songLabel.text
-            
-            // Add favorite song to favorite songs.
             italianRadio.addFavorite(song: song!)
             
         } else if songLabel.text != nil && sender.isSelected == true {
-            
-            // Remove song from favorite songs.
             sender.isSelected = false
             
-            // Set song label.
             let song = songLabel.text
-            
-            // Remove song from array.
             italianRadio.removeFavorite(song: song!)
             
         } else if songLabel.text == nil {
-            
-            // Animate play button.
             animateButton(button: playPauseButton)
             
         } else {
-            
-            // Animate play button.
             animateButton(button: playPauseButton)
         }
     }
--- radiofonico/Models/RadioModel.swift
+++ radiofonico/Models/RadioModel.swift
@@ -11,7 +11,7 @@ import AVFoundation
 
 var audioPlayer: AVAudioPlayer?
 
-class ItalianRadioModel {
+class RadioModel {
     
     // MARK: - Variables
     
@@ -21,29 +21,25 @@ class ItalianRadioModel {
     
     let defaultSongLabel = ""
     let defaultArtistLabel = ""
-    
     var defaultSong = "Lucio Dalla-Washington.mp3"
+    
     var song = ""
+    var songLabel = ""
     var isPlaying = false
     
     // MARK: - Song Selection & Song Label Functions
     
     func setSongLabel(song: String, songLabel: UILabel, artistLabel: UILabel) {
-        
         if song.contains("-") {
             
             let delimiter = "-"
-            
-            // Song filename format: Lucio Dalla-Washington.mp3
             let songInfo = song.components(separatedBy: delimiter)
             
             let song = songInfo[1]
             let artist = songInfo[0]
             
-            // Remove .mp3 from song string
             let modifiedSong = song.replacingOccurrences(of: ".mp3", with: "", options: [.caseInsensitive, .regularExpression])
             
-            // Set UILabel text
             songLabel.text = modifiedSong
             artistLabel.text = artist
         }
@@ -51,16 +47,12 @@ class ItalianRadioModel {
     
     // MARK: - Audio Player
     
-    func playSound(_ soundName: String) {
-        
+    func playSong(_ soundName: String) {
         let path = Bundle.main.path(forResource: soundName, ofType:nil)!
-        
         let url = URL(fileURLWithPath: path)
         
         do {
-            // Create the audio player
             audioPlayer = try AVAudioPlayer(contentsOf: url)
-            // Play the sound effect
             audioPlayer?.play()
         } catch {
             print("Could not summon audio player")
@@ -69,8 +61,32 @@ class ItalianRadioModel {
     
     // MARK: - Radio Control Functions
     
-    func playPreviousSong() -> String {
+    func playPauseRadio(sender: UIButton) -> String {
+        sender.isSelected.toggle()
         
+        if sender.isSelected == true {
+            isPlaying = true
+            
+            if song != "" {
+                playSong(song)
+                
+            } else {
+                song = italianRadioSongs[0]
+                playSong(song)
+            }
+            return song
+        }
+        
+        if sender.isSelected == false {
+            isPlaying = false
+            audioPlayer?.pause()
+            
+            return song
+        }
+        return defaultSongLabel
+    }
+    
+    func playPreviousSong() -> String {
         if isPlaying == true {
             
             // Get index of current song.
@@ -81,63 +97,30 @@ class ItalianRadioModel {
             
             // Reset index at end of array.
             if previousSongIndex > italianRadioSongs.count - 1 || previousSongIndex < 0 {
-                
                 previousSongIndex = italianRadioSongs.endIndex - 1
-                
                 let previousSong = italianRadioSongs[previousSongIndex]
                 
                 song = previousSong
-                
-                playSound(song)
+                playSong(song)
                 
                 return song
                 
             } else {
-                
-                // Play previous song.
                 let previousSong = italianRadioSongs[previousSongIndex]
                 
                 song = previousSong
-                
-                playSound(song)
+                playSong(song)
                 
                 return song
             }
             
         } else {
-            
             return defaultSongLabel
         }
     }
     
-    func radioOnOff(sender: UIButton) -> String {
-        
-        // Toggle radio state.
-        sender.isSelected.toggle()
-        
-        if sender.isSelected == true {
-            
-            isPlaying = true
-            song = italianRadioSongs[0]
-            playSound(song)
-            
-            return song
-            
-        } else if sender.isSelected == false {
-            
-            isPlaying = false
-            audioPlayer?.pause()
-            
-            return song
-        }
-        
-        return defaultSongLabel
-    }
-    
     func playNextSong() -> String {
-        
         if isPlaying == true {
-            
             // Get index of current song.
             let songIndex = italianRadioSongs.firstIndex(of: "\(song)")
             
@@ -148,25 +131,22 @@ class ItalianRadioModel {
             if nextSongIndex > italianRadioSongs.count - 1 {
                 nextSongIndex = 0
                 let nextSong = italianRadioSongs[nextSongIndex]
+                
                 song = nextSong
-                playSound(song)
+                playSong(song)
+                
                 return song
                 
             } else {
-                
-                // Play next song.
                 let nextSong = italianRadioSongs[nextSongIndex]
                 
                 song = nextSong
-                
-                playSound(song)
+                playSong(song)
                 
                 return song
             }
             
         } else {
-            
-            // Add animation to "Press play to vibe".
             return defaultSongLabel
         }
     }
@@ -174,34 +154,21 @@ class ItalianRadioModel {
     // MARK: - Favorite Functions
     
     func addFavorite(song: String) {
-        
-        // Filter array for song index.
         let indexArray = italianRadioSongs.indices.filter { italianRadioSongs[$0].localizedCaseInsensitiveContains("\(song)") }
-        
-        // Retrieve index from filtered array.
         let index = indexArray[0]
-        
-        // Find favorite song.
         let favorite = italianRadioSongs[index]
         
-        // Add song to favorites.
         myFavorites.append(favorite)
     }
     
     func removeFavorite(song: String) {
-        
-        // Filter array for song index.
         let indexArray = myFavorites.indices.filter { myFavorites[$0].localizedCaseInsensitiveContains("\(song)") }
-        
-        // Retrieve index from filtered array.
         let index = indexArray[0]
         
-        // Remove song from favorites.
         myFavorites.remove(at: index)
     }
     
     func checkFavorite(song: String) -> Bool {
-        
         myFavorites.contains(song)
     }
 }
